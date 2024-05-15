52cfd5a6331c3cb352a0376a77583eab55ffee25
Rodrigo <roalgoal@gmail.com>
2024-05-15 12:22:37 +0200

    added rpi IP address as env variable in compose

--- run_zenoh_bridge_rpi.sh
+++ run_zenoh_bridge_rpi.sh
@@ -3,7 +3,17 @@ export ROS_DISTRO=humble
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///var/lib/theconstruct.rrl/cyclonedds.xml
 
+# Get the IP address from the environment variable
+RPI4_IP=${RPI4_IP:-}
+if [ -z "$RPI4_IP" ]; then
+    echo "Error: RPI4_IP environment variable is not set."
+    exit 1
+fi
+
+# Path to the Zenoh configuration file
+CONFIG_FILE="/app/rpi_config.json5"
+
 # Run the process
-/zenoh-bridge-ros2dds -c /app/rpi_config.json5
+/zenoh-bridge-ros2dds -c $CONFIG_FILE
 
 echo "zenoh-bridge-ros2dds that captures RPi DDS messages has started."
