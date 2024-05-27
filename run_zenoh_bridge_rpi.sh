3d8e4b52504d740788288dc6e2a1d9559ac7130f
Rodrigo <roalgoal@gmail.com>
2024-05-27 14:19:40 +0200

    removed RPI4_IP environment variable

--- run_zenoh_bridge_rpi.sh
+++ run_zenoh_bridge_rpi.sh
@@ -4,18 +4,8 @@ export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///var/lib/theconstruct.rrl/cyclonedds.xml
 export ROS_DOMAIN_ID=0
 
-# Get the IP address from the environment variable
-RPI4_IP=${RPI4_IP:-}
-if [ -z "$RPI4_IP" ]; then
-    echo "Error: RPI4_IP environment variable is not set."
-    exit 1
-fi
-
 # Path to the Zenoh configuration file
 CONFIG_FILE="/app/rpi_config.json5"
-
-# Replace the placeholder in the configuration file
-sed -i "s/__RPI4_IP_PLACEHOLDER__/$RPI4_IP/g" $CONFIG_FILE
 
 # Run the process
 /zenoh-bridge-ros2dds -c $CONFIG_FILE
